2a79b6d7b9b6998a72b227ac7d1408cf4f648805
roychanckc <aasdpph8@gmail.com>
2022-05-15 02:49:04 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React abc ddfsfa
+          Learn React with Docker
         </a>
       </header>
     </div>
